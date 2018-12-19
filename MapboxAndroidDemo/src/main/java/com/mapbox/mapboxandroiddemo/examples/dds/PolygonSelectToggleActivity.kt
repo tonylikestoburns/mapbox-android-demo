dfcef9156cfeaea15d0cc0d54b90749416d9a9d0
Langston Smith <langston.smith@mapbox.com>
2018-12-18 23:04:07 -0800

    additions

--- MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/dds/PolygonSelectToggleActivity.kt
+++ MapboxAndroidDemo/src/main/java/com/mapbox/mapboxandroiddemo/examples/dds/PolygonSelectToggleActivity.kt
@@ -1,25 +1,36 @@
 package com.mapbox.mapboxandroiddemo.examples.dds
 
+import android.content.Context
 import android.graphics.Color
+import android.graphics.PointF
+import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.widget.Toast
+import com.mapbox.geojson.Feature
 import com.mapbox.geojson.FeatureCollection
 import com.mapbox.mapboxandroiddemo.R
 import com.mapbox.mapboxsdk.Mapbox
 import com.mapbox.mapboxsdk.geometry.LatLng
 import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.Style
+import com.mapbox.mapboxsdk.style.expressions.Expression.*
 import com.mapbox.mapboxsdk.style.layers.FillLayer
 import com.mapbox.mapboxsdk.style.layers.LineLayer
 import com.mapbox.mapboxsdk.style.layers.PropertyFactory.*
 import com.mapbox.mapboxsdk.style.sources.GeoJsonSource
 import kotlinx.android.synthetic.main.activity_dds_polygon_select_toggle.*
+import java.lang.ref.WeakReference
 
-class PolygonSelectToggleActivity : AppCompatActivity() , MapboxMap.OnMapClickListener {
+class PolygonSelectToggleActivity : AppCompatActivity(), MapboxMap.OnMapClickListener {
 
     private var mapboxMap: MapboxMap? = null
+    private val FILL_LAYER_ID = "FILL_LAYER_ID"
+    private val LINE_LAYER_ID = "LINE_LAYER_ID"
+    private val PROPERTY_SELECTED = "selected"
+    private val NEIGHBORHOOD_NAME_PROPERTY = "neighborhood_name"
+    private var featureCollection: FeatureCollection? = null
+    private var geoJsonSource: GeoJsonSource? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,28 +44,11 @@ class PolygonSelectToggleActivity : AppCompatActivity() , MapboxMap.OnMapClickLi
 
         mapView.onCreate(savedInstanceState)
         mapView.getMapAsync { mapboxMap ->
-            this.mapboxMap = mapboxMap
-
             mapboxMap.setStyle(Style.LIGHT) {
 
-                val geoJsonSource = GeoJsonSource(
-                        "polygon-source",
-                        FeatureCollection.fromJson(
-                                loadGeoJsonFromAsset("new-orleans-neighborhoods.geojson")!!)
-                )
-                mapboxMap.style?.addSource(geoJsonSource)
+                this.mapboxMap = mapboxMap
 
-                val fillLayer = FillLayer("fill-layer-id", "polygon-source")
-                fillLayer.withProperties(
-                        fillColor(Color.GRAY),
-                        fillOpacity(.15f))
-                mapboxMap.style?.addLayerBelow(fillLayer,"place-city-sm")
-
-                val lineLayer = LineLayer("layer-layer-id", "polygon-source")
-                lineLayer.withProperties(
-                        lineColor(Color.GRAY),
-                        lineWidth(2f))
-                mapboxMap.style?.addLayerBelow(lineLayer,"place-city-sm")
+                LoadGeoJsonDataTask(this).execute()
 
                 mapboxMap.addOnMapClickListener(this)
             }
@@ -62,33 +56,171 @@ class PolygonSelectToggleActivity : AppCompatActivity() , MapboxMap.OnMapClickLi
     }
 
     override fun onMapClick(point: LatLng): Boolean {
-        val pixel = mapboxMap?.getProjection()?.toScreenLocation(point)
-        val features = mapboxMap?.queryRenderedFeatures(pixel!!,
-                "fill-layer-id","layer-layer-id")
-        if (features!!.size > 0) {
+        return handleClickIcon(mapboxMap?.getProjection()!!.toScreenLocation(point))
+        /*return true*/
+    }
+
+    /**
+     * Sets up all of the sources and layers needed for this example
+     *
+     * @param collection the FeatureCollection to set equal to the globally-declared FeatureCollection
+     */
+    fun setUpData(collection: FeatureCollection) {
+        if (mapboxMap == null) {
+            return
+        }
+        geoJsonSource = GeoJsonSource(
+                "polygon-source", featureCollection)
+
+        mapboxMap?.style?.addSource(geoJsonSource!!)
+
+        val fillLayer = FillLayer(FILL_LAYER_ID, "polygon-source")
+        fillLayer.withProperties(
+                fillColor(
+                        match(get(PROPERTY_SELECTED),Color.parseColor(""),
+                                stop("true", rgb(251, 176, 59)),
+                                stop("Black", rgb(34, 59, 83)))),
+
+                fillOpacity(.15f))
+        mapboxMap?.style?.addLayerBelow(fillLayer, "place-city-sm")
+
+        val lineLayer = LineLayer(LINE_LAYER_ID, "polygon-source")
+        lineLayer.withProperties(
+                lineColor(Color.GRAY),
+                lineWidth(2f))
+        mapboxMap?.style?.addLayerBelow(lineLayer, "place-city-sm")
+
+        featureCollection = collection
+    }
+    /**
+     * This method handles click events for SymbolLayer symbols.
+
+     * When a SymbolLayer icon is clicked, we moved that feature to the selected state.
+     * @param screenPoint the point on screen clicked
+     */
+    private fun handleClickIcon(screenPoint: PointF): Boolean {
+        val features = mapboxMap?.queryRenderedFeatures(screenPoint, FILL_LAYER_ID,
+                LINE_LAYER_ID)
+        if (features?.isEmpty() == false) {
             Toast.makeText(this, features[0].getStringProperty("neighborhood_name"),
                     Toast.LENGTH_SHORT).show()
+
+            val name = features.get(0).getStringProperty(NEIGHBORHOOD_NAME_PROPERTY)
+            val featureList = featureCollection?.features()
+            for (i in featureList!!.indices) {
+                if (featureList.get(i).getStringProperty(NEIGHBORHOOD_NAME_PROPERTY) == name) {
+                    if (featureSelectStatus(i)) {
+                        setFeatureSelectState(featureList.get(i), false)
+                    } else {
+                        setSelected(i)
+                    }
+                }
+            }
+            return true
+        } else {
+            return false
         }
-        return true
     }
 
-    private fun loadGeoJsonFromAsset(filename: String): String? {
-        try {
-            // Load GeoJSON file
-            val `is` = assets.open(filename)
-            val size = `is`.available()
-            val buffer = ByteArray(size)
-            `is`.read(buffer)
-            `is`.close()
-            return String(buffer)
+    /**
+     * Selects the state of a feature
+     *
+     * @param feature the feature to be selected.
+     */
+    private fun setFeatureSelectState(feature: Feature, selectedState: Boolean) {
+        feature.properties()!!.addProperty(PROPERTY_SELECTED, selectedState)
+        refreshSource()
+    }
 
-        } catch (exception: Exception) {
-            Log.e("PolygonSelectToggle", "Exception Loading GeoJSON: " + exception.toString())
-            exception.printStackTrace()
-            return null
+    /**
+     * Checks whether a Feature's boolean "selected" property is true or false
+     *
+     * @param index the specific Feature's index position in the FeatureCollection's list of Features.
+     * @return true if "selected" is true. False if the boolean property is false.
+     */
+    private fun featureSelectStatus(index: Int): Boolean {
+        return if (featureCollection == null) {
+            false
+        } else featureCollection?.features()!![index].getBooleanProperty(PROPERTY_SELECTED)
+    }
+
+    /**
+     * Updates the display of data on the map after the FeatureCollection has been modified
+     */
+    private fun refreshSource() {
+        if (geoJsonSource != null && featureCollection != null) {
+            geoJsonSource?.setGeoJson(featureCollection)
+        }
+    }
+
+    /**
+     * Set a feature selected state.
+     *
+     * @param index the index of selected feature
+     */
+    private fun setSelected(index: Int) {
+        val feature = featureCollection?.features()!![index]
+        setFeatureSelectState(feature, true)
+        refreshSource()
+    }
+
+    /**
+     * AsyncTask to load data from the assets folder.
+     */
+    private class LoadGeoJsonDataTask internal
+    constructor(activity: PolygonSelectToggleActivity) :
+            AsyncTask<Void, Void, FeatureCollection>() {
+
+        private val activityRef: WeakReference<PolygonSelectToggleActivity>
+
+        init {
+            this.activityRef = WeakReference(activity)
         }
 
+        override fun doInBackground(vararg params: Void): FeatureCollection? {
+            val activity = activityRef.get() ?: return null
+
+            val geoJson = loadGeoJsonFromAsset(activity,
+                    "new-orleans-neighborhoods.geojson")
+            return FeatureCollection.fromJson(geoJson)
+        }
+
+        override fun onPostExecute(featureCollection: FeatureCollection?) {
+            super.onPostExecute(featureCollection)
+            val activity = activityRef.get()
+            if (featureCollection == null || activity == null) {
+                return
+            }
+
+            // This example runs on the premise that each GeoJSON Feature has a "selected" property,
+            // with a boolean value. If your data's Features don't have this boolean property,
+            // add it to the FeatureCollection 's features with the following code:
+            for (singleFeature in featureCollection.features()!!) {
+                singleFeature.addBooleanProperty("selected", false)
+            }
+
+            activity.setUpData(featureCollection)
+        }
+
+        companion object {
+
+            internal fun loadGeoJsonFromAsset(context: Context, filename: String): String {
+                try {
+                    // Load GeoJSON file
+                    val `is` = context.assets.open(filename)
+                    val size = `is`.available()
+                    val buffer = ByteArray(size)
+                    `is`.read(buffer)
+                    `is`.close()
+                    return String(buffer)
+                } catch (exception: Exception) {
+                    throw RuntimeException(exception)
+                }
+
+            }
+        }
     }
+
     override fun onStart() {
         super.onStart()
         mapView.onStart()
